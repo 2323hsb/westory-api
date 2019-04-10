7c09a9120f557b0b67ece0847b0f7402912a97c4
jung <2323hsb@gmail.com>
2019-04-10 15:54:23 +0900

    댓글 기능 복구

--- westory_api/apis/views.py
+++ westory_api/apis/views.py
@@ -40,85 +40,6 @@ class ApiRoot(views.APIView):
         })
 
 
-# class UserAPI(generics.ListAPIView):
-#     serializer_class = UserSerializer
-
-#     def get_queryset(self):
-#         current_user = self.request.user
-#         if current_user:
-#             user = User.objects.filter(email=current_user)
-#         return user
-
-
-# class PostAPI(generics.ListCreateAPIView):
-#     serializer_class = PostSerializer
-#     queryset = Post.objects.all().order_by('-created_date')
-#     pagination_class = LimitOffsetPagination
-
-#     def perform_create(self, serializer):
-#         serializer.save(user=self.request.user)
-
-
-# class LikesAPI(views.APIView):
-#     def get(self, request, *args, **kwargs):
-#         if not 'post_id' in self.request.query_params:
-#             like_post_list = self.request.user.like_posts.all()
-#             results = []
-#             for like_post in like_post_list:
-#                 results.append(like_post.pk)
-
-#             return Response(data=results)
-#         else:
-#             post_id = self.request.query_params.get('post_id')
-#             try:
-#                 target_post = Post.objects.get(id=post_id)
-#                 like_count = target_post.like_users.all().count()
-#                 is_like = False
-#                 if self.request.user in target_post.like_users.all():
-#                     is_like = True
-#                 results = {}
-#                 results['like_count'] = like_count
-#                 results['is_like'] = is_like
-#                 return Response(data=results)
-#             except Post.DoesNotExist:
-#                 raise ValidationError("invaild post id")
-
-#     def post(self, request, *args, **kwargs):
-#         if not 'post_id' in self.request.data:
-#             raise ValidationError("You need 'post_id'")
-#         post_id = self.request.data['post_id']
-#         try:
-#             target_post = Post.objects.get(id=post_id)
-#             is_like = self.request.data['is_like']
-#             if is_like == 'true':
-#                 target_post.like_users.add(self.request.user)
-#             else:
-#                 target_post.like_users.remove(self.request.user)
-#             target_post.save()
-#         except Post.DoesNotExist:
-#             raise ValidationError("invaild post id")
-
-#         return Response(data="a")
-
-
-# class ReplyAPI(generics.ListCreateAPIView):
-#     serializer_class = ReplySerializer
-
-#     def get_queryset(self):
-#         post_id = self.request.query_params.get('post_id')
-#         return Reply.objects.filter(post=post_id)
-
-#     def perform_create(self, serializer):
-#         if not 'post_id' in self.request.data:
-#             raise ValidationError("You need 'post_id'")
-#         post_id = self.request.data['post_id']
-#         try:
-#             target_post = Post.objects.get(id=post_id)
-#             serializer.save(user=self.request.user, post=target_post)
-#         except Post.DoesNotExist:
-#             raise ValidationError("invaild post id")
-
-
 class UploadImageAPI(generics.CreateAPIView):
     queryset = UploadImage.objects.all()
     serializer_class = ImageSerializer
@@ -135,9 +56,6 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     permission_classes = (IsAuthenticated | ReadOnly,)
 
-    def retrieve(self, request, *args, **kwargs):
-        return super().retrieve(request, *args, **kwargs)
-
 
 class StoryViewSet(viewsets.ModelViewSet):
     queryset = Story.objects.all().order_by('-created_date')
@@ -147,19 +65,6 @@ class StoryViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-
-    # def list(self, request, *args, **kwargs):
-    #     queryset = self.filter_queryset(self.get_queryset())
-    #     # print(queryset.count())
-
-    #     page = self.paginate_queryset(queryset)
-    #     if page is not None:
-    #         serializer = self.get_serializer(page, many=True)
-    #         print(serializer.data)
-    #         return self.get_paginated_response(serializer.data)
-
-    #     serializer = self.get_serializer(queryset, many=True)
-    #     return Response(serializer.data)
 
     def retrieve(self, request, *args, **kwargs):
         story = self.get_object()
@@ -187,6 +92,14 @@ class StoryViewSet(viewsets.ModelViewSet):
         except Story.DoesNotExist:
             raise ValidationError("invaild story id")
 
+    @detail_route(methods=['get'])
+    def comments(self, request, pk=None):
+        story = self.get_object()
+        comments = story.comments.all().order_by('-created_date')
+        serializer = CommentSerializer(
+            comments, context={'request': request}, many=True)
+        return Response(serializer.data)
+
     @detail_route(methods=['post'])
     def add_comment(self, request, pk=None):
         serializer = CommentSerializer(data=request.data)
@@ -195,7 +108,7 @@ class StoryViewSet(viewsets.ModelViewSet):
             return Response(data={
                 'status': 'add comment success',
             })
-        else: 
+        else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
